22533ccd433a50aa062d2fbdf5cee37f38c208f2
Leonardo Yosua <workspace@yosualeo.com>
2021-03-09 13:49:20 +0700

    Initial Commit

---
+++ SwiftDrill/SwiftDrillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDrillApp.swift
+//  SwiftDrill
+//
+//  Created by Yosua Leonardo on 09/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDrillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
